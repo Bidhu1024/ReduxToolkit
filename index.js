de1198761610cdb7b02c73f024407999250b1809
Bidhu1024 <bidhu1024av@gmail.com>
2023-04-06 10:59:08 +0530

    rr

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ const actionCreator = ()=>{
         quantity:1
     }
 }
+
+
+const initialState = {
+    numOfCakes:10
+}
+
+const reducer = (state=initialState, action)=>{
+    switch(action.type){
+        case BUY_CAKE:
+            return{
+                numOfCakes:state.numOfCakes -1
+            }
+            default: 
+            return state
+    }
+}
